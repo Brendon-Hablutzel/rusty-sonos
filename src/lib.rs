80e7a249ed8379117f3331df30d147a17e017f1b
Brendon-Hablutzel <booklvrbren@gmail.com>
2023-12-22 21:27:01 -0500

    edit examples to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,26 @@
 //!
 //! Connecting to a Sonos speaker:
 //! ```rust
-//! let ip_addr = "192.168.1.0";
+//! let ip_addr = Ipv4Addr::from_str("192.168.1.0").unwrap();
 //!
 //! let speaker = Speaker::new(ip_addr).await.unwrap();
 //! ```
 //!
 //! Discovering speakers on the current network:
 //! ```rust
+//! // search for 2 seconds, with a read timeout of 5 seconds
 //! let devices = discover_devices(2, 5).await.unwrap();
 //!
 //! for device in devices {
 //!    println!("{}, {}", device.friendly_name, device.room_name)
 //! }
+//! ```
+//!
+//! Get information about a speaker at a certain IP address
+//! ```rust
+//! let ip_addr = Ipv4Addr::from_str("192.168.1.0").unwrap();
+//!
+//! let info = get_speaker_info(ip_addr).await.unwrap();
 //! ```
 
 pub mod discovery;
@@ -25,8 +33,3 @@ pub mod responses;
 mod services;
 pub mod speaker;
 mod xml;
-
-// LOWERCASE ALL ERRORS
-// SEE IF 'kind's CAN BE MERGED INTO STRUCTS
-// IMPLEMENT DISPLAY AND ERROR FOR ALL ERRORS
-// DOCUMENT ERRORS
